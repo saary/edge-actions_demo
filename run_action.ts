8c8d5963777a960e7fee73f462f12c188a2f668f
Saar Yahalom <syahalom@gmail.com>
2023-07-11 18:45:45 +0300

    Update run_action.ts

--- run_action.ts
+++ run_action.ts
@@ -3,8 +3,7 @@ import { add } from 'https://raw.githubusercontent.com/saary/edge-actions_demo/m
 export async function runAction(action: string, data: object) {
   switch (action) {
     case 'add':
-      // const res = await add(data);
-      return { result: 3 }
+      return { result: await add(data) };
     default:
       return { error: 'No such action found' };
   }
